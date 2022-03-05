895c89836647a66139021cc112baccfb6c2c1504
Luiz Fernando Okada <82292871+luizokada@users.noreply.github.com>
2022-03-05 00:37:52 -0300

    Update bancoDeDados.java

--- src/base/bancoDeDados.java
+++ src/base/bancoDeDados.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 
 public class bancoDeDados {
 	public Connection recuperaConexao() throws SQLException {
-		return  DriverManager.getConnection("jdbc:mysql://localhost:3306/teste?useTimezone=true&serverTimezone=UTC", "root", "Omegacode@01");
+		return  DriverManager.getConnection("jdbc:mysql://localhost:3306/teste?useTimezone=true&serverTimezone=UTC", "root", "Senha");
 	}
 
 }
